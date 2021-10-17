7d9c2c41dc0b5b332a20e056c48d77cd32028cb0
NoYouNotUrUncle <74073421+noyounoturuncle@users.noreply.github.com>
2021-10-16 20:39:17 -0400

    Fix migration to save

--- website/migrations/0003_addMembers.py
+++ website/migrations/0003_addMembers.py
@@ -6,10 +6,10 @@ def make_models(apps, schema_editor):
     vp = ExecRole(name='Vice President', color='#A652BB').save()
     genExec = ExecRole(name='General Executive', color='#00D166').save()
     compExec = ExecRole(name='Competitive Executive', color='#0099E1').save()
-    catBoyEnMing = ExecRole(name='Cat Boy En Ming', color='#FF1694')
+    catBoyEnMing = ExecRole(name='Cat Boy En Ming', color='#FF1694').save()
 
     Exec = apps.get_model('website', 'Exec')
-    bsscc = Exec(name='Bayview Computer Club Executive Team')
+    bsscc = Exec(name='Bayview Computer Club Executive Team').save()
 
 class Migration(migrations.Migration):
     dependencies = [
